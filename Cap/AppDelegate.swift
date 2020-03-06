c7af050ac55ccad5f4c3f151265398ff7bd041b6
Johann Olivares <johannolivares102@gmail.com>
2020-03-06 11:36:28 -0800

    update

--- Cap/AppDelegate.swift
+++ Cap/AppDelegate.swift
@@ -12,6 +12,7 @@ import Parse
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    var window: UIWindow?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
@@ -22,7 +23,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 configuration.server = "https://fast-journey-83048.herokuapp.com/parse"
             })
         )
-
+        
+        if PFUser.current() != nil {
+            let main = UIStoryboard(name: "Main", bundle: nil)
+            let feedNavigationController = main.instantiateViewController(withIdentifier: "feedNavController")
+            
+            window?.rootViewController = feedNavigationController
+        }
+        
         return true
     }
 
--- Cap/FeedViewController.swift
+++ Cap/FeedViewController.swift
@@ -64,6 +64,19 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         return cell
     }
     
+    
+    @IBAction func onLogoutButton(_ sender: Any) {
+        PFUser.logOut()
+        
+        let main = UIStoryboard(name: "Main", bundle: nil)
+        let loginViewController =  main.instantiateViewController(withIdentifier: "LoginViewController")
+        
+        let delegate = UIApplication.shared.delegate as! AppDelegate
+        
+        delegate.window?.rootViewController = loginViewController
+    }
+    
+    
     /*
     // MARK: - Navigation
 
